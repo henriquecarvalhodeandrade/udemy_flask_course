248098e0408b19ed4d46aa13f724fcfbdec04162
Henrique C. de Andrade <rique9906@gmail.com>
2025-11-21 16:54:16 -0300

    aula 04
    criando tabela curso pelo terminal e utilizando migrate para versionamento

--- pythondev/13 - Desenvolvimento de APIs/api/__init__.py
+++ pythondev/13 - Desenvolvimento de APIs/api/__init__.py
@@ -1,9 +1,11 @@
-from urllib.response import addbase
-
 from flask import Flask
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import pymysql
+
+# Corrige driver do MySQL no Windows
+pymysql.install_as_MySQLdb()
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -12,6 +14,5 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 api = Api(app)
 
-
 from .views import curso_views
-
+from .models import curso_model
