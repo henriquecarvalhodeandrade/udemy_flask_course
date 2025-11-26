f2931fdfb93befdc41fbb826d5c77b293b58bc3e
Henrique C. de Andrade <rique9906@gmail.com>
2025-11-26 18:30:27 -0300

    aula 10
    implementamos paginação

--- pythondev/14 - Utilizando Relacionamentos em APIs/api/schemas/formacao_schema.py
+++ pythondev/14 - Utilizando Relacionamentos em APIs/api/schemas/formacao_schema.py
@@ -1,10 +1,10 @@
 from api import ma
 from ..models import formacao_model
 from marshmallow import fields
-from ..schemas import curso_schema
-
+from ..schemas import curso_schema, professor_schema
 
 class FormacaoSchema(ma.SQLAlchemyAutoSchema):
+    professores = ma.Nested(professor_schema.ProfessorSchema, many=True, only=('id','nome'))
     class Meta:
         model = formacao_model.Formacao
         load_instance = True
